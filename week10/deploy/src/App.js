ae563c847b5a082e356b0904968bd90e72a2a78e
Alison Law <alisparklaw@gmail.com>
2020-02-27 16:08:15 +0000

    second commit

--- week10/deploy/src/App.js
+++ week10/deploy/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       
-         <h1>Deploy me</h1> 
+         <h1>Deploy me!</h1> 
        
       </header>
     </div>
